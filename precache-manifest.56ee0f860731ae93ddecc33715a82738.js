1b6d8bac6135883e0749316fc26c44eaa3f817e3
priyanka kalamegam <kpriyankaoff@gmail.com>
2020-09-28 01:25:18 +0530

    Updates

---
+++ precache-manifest.56ee0f860731ae93ddecc33715a82738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33d5151991c6e3d00fd0a9e8911843b",
+    "url": "/todo-items/index.html"
+  },
+  {
+    "revision": "933967b1fee348b91284",
+    "url": "/todo-items/static/css/main.26bdc978.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/todo-items/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-items/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933967b1fee348b91284",
+    "url": "/todo-items/static/js/main.242cc029.chunk.js"
+  },
+  {
+    "revision": "b24c809782822eb60ec3",
+    "url": "/todo-items/static/js/runtime-main.3988fdad.js"
+  }
+]);
